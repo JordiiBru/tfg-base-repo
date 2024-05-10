4865b527a3a0cf08564b936e0353173249d11ec9
jordi.bru <jordi.bru@mango.com>
2024-05-10 19:50:46 +0200

    feat: comments on PR for tf

--- dev/apps/testing-1/main.tf
+++ dev/apps/testing-1/main.tf
@@ -1,13 +1,3 @@
-resource "aws_s3_bucket" "testing_1_lala" {
-  bucket = "testing-1-lala-lala"
-
-  tags = {
-    Terraform = var.terraform
-    Ownership = var.owner
-    Stage     = var.stage
-  }
-}
-
 resource "aws_s3_bucket" "testing_2_lala" {
   bucket = "testing-2-lala-lala"
 
