c9cfd00ce6e2751237071273c23ed6a317d6c9f8
jordi.bru <jordi.bru@mango.com>
2024-05-10 20:30:10 +0200

    color test

--- dev/apps/testing-1/main.tf
+++ dev/apps/testing-1/main.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "testing_2_lala" {
     Stage     = var.stage
   }
 }
+
+resource "aws_s3_bucket" "testing_3_lala" {
+  bucket = "testing-3-lala-lala"
+
+  tags = {
+    Terraform = var.terraform
+    Ownership = var.owner
+    Stage     = var.stage
+  }
+}
