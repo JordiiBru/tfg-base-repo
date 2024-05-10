6493bcde62bac8b5d7bb6a8c321fbed89506b8b0
jordi.bru <jordi.bru@mango.com>
2024-05-10 19:28:27 +0200

    add: improving plan comment

--- dev/apps/testing-1/main.tf
+++ dev/apps/testing-1/main.tf
@@ -7,3 +7,13 @@ resource "aws_s3_bucket" "testing_1_lala" {
     Stage     = var.stage
   }
 }
+
+resource "aws_s3_bucket" "testing_2_lala" {
+  bucket = "testing-2-lala-lala"
+
+  tags = {
+    Terraform = var.terraform
+    Ownership = var.owner
+    Stage     = var.stage
+  }
+}
