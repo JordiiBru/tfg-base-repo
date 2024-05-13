33e206140a3e53d4e9b395566d839184d97624f4
jordi.bru <jordi.bru@mango.com>
2024-05-13 23:23:29 +0200

    lolo

---
+++ dev/apps/testing-2/main.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "testing_2_lala" {
+  bucket = "testing-3-lala-lala"
+
+  tags = {
+    Terraform = var.terraform
+    Ownership = var.owner
+    Stage     = var.stage
+  }
+}
---
+++ dev/apps/testing-2/terraform.tfvars
@@ -0,0 +1,3 @@
+terraform = true
+stage     = "dev"
+owner     = "jordi"
