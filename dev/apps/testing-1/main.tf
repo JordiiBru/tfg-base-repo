6d9f24ea6fa840c3b26e4d760f39803cac4e56c2
jordi.bru <jordi.bru@mango.com>
2024-05-14 23:08:02 +0200

    fix: clean s3

--- dev/apps/testing-1/main.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_s3_bucket" "testing_1_lala" {
-  bucket = "testing-3-lala-lala"
-
-  tags = {
-    Terraform = var.terraform
-    Ownership = var.owner
-    Stage     = var.stage
-  }
-}
