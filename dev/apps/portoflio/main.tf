82c92aad9bb51645b8ee4c7e585f683b7d54863f
Jordii Bru <71638828+JordiiBru@users.noreply.github.com>
2024-05-27 23:49:53 +0200

    Feature/add portfolio dev (#41)
    * add: portfolio in dev

---
+++ dev/apps/portoflio/main.tf
@@ -0,0 +1,11 @@
+module "static" {
+  source = "git@github.com:JordiiBru/tf-static-website.git?ref=v0.0.1"
+
+  # Required variables
+  stage   = var.stage
+  owner   = var.owner
+  purpose = "portfolio"
+
+  # Custom variables
+  bucket_versioning = true
+}
