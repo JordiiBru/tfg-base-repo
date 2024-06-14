2d2811a2bf6bcd96f9c7cd45f588df7554b64acd
jordi.bru <jordi.bru@mango.com>
2024-06-14 22:28:32 +0200

    fix: restore files

--- dev/apps/portfolio/main.tf
+++ dev/apps/portfolio/main.tf
@@ -1,11 +1,11 @@
-# module "static" {
-#   source = "git@github.com:JordiiBru/tf-static-website.git?ref=v0.0.1"
+module "static" {
+  source = "git@github.com:JordiiBru/tf-static-website.git?ref=v0.0.1"
 
-#   # Common variables
-#   stage   = var.stage
-#   owner   = var.owner
-#   purpose = "portfolio"
+  # Common variables
+  stage   = var.stage
+  owner   = var.owner
+  purpose = "portfolio"
 
-#   # Custom variables
-#   bucket_versioning = true
-# }
+  # Custom variables
+  bucket_versioning = true
+}
