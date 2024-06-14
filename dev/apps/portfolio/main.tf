9cc94338c41312aaa2174613669e31be094f3fde
jordi.bru <jordi.bru@mango.com>
2024-06-14 22:30:55 +0200

    fix: restore

--- dev/apps/portfolio/main.tf
+++ dev/apps/portfolio/main.tf
@@ -1,7 +1,7 @@
 module "static" {
   source = "git@github.com:JordiiBru/tf-static-website.git?ref=v0.0.1"
 
-  # Common variables
+  # Required variables
   stage   = var.stage
   owner   = var.owner
   purpose = "portfolio"
