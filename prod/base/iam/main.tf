3f9b9552bb6cf2a389aa090a1e990997d26ed42e
Jordii Bru <71638828+JordiiBru@users.noreply.github.com>
2024-08-22 19:40:08 +0200

    feat(): add permissions to cicd role for portfolio repo (#57)

--- prod/base/iam/main.tf
+++ prod/base/iam/main.tf
@@ -15,7 +15,10 @@ resource "aws_iam_role" "github_actions_role" {
             "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
           },
           "StringLike" : {
-            "token.actions.githubusercontent.com:sub" : "repo:JordiiBru/tfg-base-repo:*"
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:JordiiBru/tfg-base-repo:*",
+              "repo:JordiiBru/personal-portfolio:*"
+            ]
           }
         }
       }
